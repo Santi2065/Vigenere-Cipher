64fc245eea007d9e72db383430bfeb1064f6c117
Santiago Groba <31144477+Santi2065@users.noreply.github.com>
2023-04-11 11:06:35 -0300

    Create encriptador.py
    Se debe escribir un programa que le pida al usuario el path a un archivo en texto plano, por ejemplo plain.txt y, con una clave solicitada al usuario, encripte el texto mediante cifrado de Vigenère y lo guarde en un nuevo archivo elegido por el usuario, por ejemplo encrypted.txt. Todos los caracteres especiales y números deben quedar como estaban en el texto plano. Las mayúsculas deben convertirse en minúsculas antes de encriptar, el texto encriptado estará entonces todo en minúscula. En este caso encriptaremos sólo las 26 letras del alfabeto inglés. El programa no debe fallar catastróficamente ante entradas insperadas del usuario, archivos vacíos o inexistentes, etc.

---
+++ encriptador.py
@@ -0,0 +1,9 @@
+'''
+Se debe escribir un programa que le pida al usuario el path a un archivo en texto plano,
+por ejemplo plain.txt y, con una clave solicitada al usuario, encripte el texto mediante
+cifrado de Vigenère y lo guarde en un nuevo archivo elegido por el usuario, por ejemplo encrypted.txt.
+Todos los caracteres especiales y números deben quedar como estaban en el texto plano.
+Las mayúsculas deben convertirse en minúsculas antes de encriptar, el texto encriptado estará entonces todo en minúscula.
+En este caso encriptaremos sólo las 26 letras del alfabeto inglés.
+El programa no debe fallar catastróficamente ante entradas insperadas del usuario, archivos vacíos o inexistentes, etc.
+'''
