1d3d72499f53496dc12a848de36da47ac31163c1
Santiago Groba <31144477+Santi2065@users.noreply.github.com>
2023-04-11 11:07:25 -0300

    Create desencriptador.py
    Se debe escribir un programa que le pida al usuario el path al archivo encriptado, por ejemplo llamado encrypted.txt, le solicite al usuario una clave y luego desencripte el texto con Cifrado de Vigenère y lo guarde en un nuevo archivo elegido por el usuario, por ejemplo llamado decrypted.txt.

---
+++ desencriptador.py
@@ -0,0 +1,5 @@
+'''
+Se debe escribir un programa que le pida al usuario el path al archivo encriptado,
+por ejemplo llamado encrypted.txt, le solicite al usuario una clave y luego 
+desencripte el texto con Cifrado de Vigenère y lo guarde en un nuevo archivo elegido por el usuario, por ejemplo llamado decrypted.txt.
+'''
